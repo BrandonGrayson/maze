391ba6d574eaf89520c4466409bdf59c12c08a72
Brandon Grayson <brandonbgrayson@gmail.com>
2020-08-04 10:37:53 -0400

    Shapes Game completed/moving on to the maze

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-//next task run git init push to a new github repo
 
-const {Engine, Render, Runner, World, Bodies} = Matter;
+
+const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -8,12 +11,16 @@ const render = Render.create({
     element: document.body,
     engine: engine, 
     options: {
+        wireframes: false,
         width: 800,
         height: 600
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}))
 
 // const shape = Bodies.rectangle(200, 200, 50, 50, {
 //     isStatic: true
@@ -29,3 +36,19 @@ const walls = [
     Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
 ];
 World.add(world, walls);
+
+//Random Shapes
+
+for(let i = 0; i < 50; i++) {
+    if(Math.random() > 0.5) {
+        World.add(
+            world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+        );
+    } else {
+        World.add(
+            world,
+            Bodies.circle(Math.random() * width, Math.random() * height, 35)
+        );
+    }   
+}
+
