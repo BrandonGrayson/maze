fdb2a39d5e67547fb16b9ab6cec2475173918002
Brandon Grayson <brandonbgrayson@gmail.com>
2020-08-07 17:29:01 -0400

    Fixed wall Bug

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ horizontals.forEach((row, rowIndex) => {
             columnIndex * unitLength + unitLength / 2,
             rowIndex * unitLength + unitLength,
             unitLength,
-            10,
+            5,
             {
                 isStatic: true
             }
@@ -132,7 +132,7 @@ verticals.forEach((row, rowIndex) => {
         const wall = Bodies.rectangle(
             columnIndex * unitLength + unitLength,
             rowIndex * unitLength + unitLength,
-            10,
+            5,
             unitLength,
             {
                 isStatic: true
