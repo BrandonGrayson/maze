c17127f7c223f3317fdd2b6b3327fd9b05f67f6a
Brandon Grayson <brandonbgrayson@gmail.com>
2020-08-09 19:35:32 -0400

    Refactored maze to be scalable in X and Y directions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 const unitLengthX = width / cellsHorizontal;
-const unitLenghtY = height / cellsVertical;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0
@@ -52,13 +52,13 @@ const shuffle = (arr) => {
     return arr;
 };
 
-const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
+const grid = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal).fill(false))
 
-const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
-const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
+const verticals = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal - 1).fill(false))
+const horizontals = Array(cellsVertical - 1).fill(null).map(() => Array(cellsHorizontal).fill(false))
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const stepThroughCell = (row, column) => {
     // if i have visited the cell at [row, column], then return
@@ -79,7 +79,7 @@ const stepThroughCell = (row, column) => {
     for(let neighbor of neighbors) {
             const [nextRow, nextColumn, direction] = neighbor;
         // See if neighbor is out of bounds
-            if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            if(nextRow < 0 || nextRow >= cellsVertical || nextColumn < 0 || nextColumn >= cellsHorizontal) {
                 continue;
             }
         // If we have visited that neighbor, then visit next neighbor
@@ -113,9 +113,9 @@ horizontals.forEach((row, rowIndex) => {
         }
 
         const wall = Bodies.rectangle(
-            columnIndex * unitLength + unitLength / 2,
-            rowIndex * unitLength + unitLength,
-            unitLength,
+            columnIndex * unitLengthX + unitLengthX / 2,
+            rowIndex * unitLengthY + unitLengthY,
+            unitLengthX,
             5,
             {
                 label: 'wall',
@@ -134,10 +134,10 @@ verticals.forEach((row, rowIndex) => {
         }
         // Determine if open argument is true and where to place walls
         const wall = Bodies.rectangle(
-            columnIndex * unitLength + unitLength,
-            rowIndex * unitLength + unitLength / 2,
+            columnIndex * unitLengthX + unitLengthX,
+            rowIndex * unitLengthY + unitLengthY / 2,
             5,
-            unitLength,
+            unitLengthY,
             {
                 label: 'wall',
                 isStatic: true
@@ -150,10 +150,10 @@ verticals.forEach((row, rowIndex) => {
 
 // Goal
 const goal = Bodies.rectangle(
-    width - unitLength / 2,
-    height - unitLength / 2,
-    unitLength * 0.7,
-    unitLength * 0.7,
+    width - unitLengthX / 2,
+    height - unitLengthY / 2,
+    unitLengthX * 0.7,
+    unitLengthY * 0.7,
     {
         label: 'goal',
         isStatic: true
@@ -163,10 +163,11 @@ World.add(world, goal);
 
 // Ball
 
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(
-    unitLength / 2,
-    unitLength / 2,
-    unitLength / 4,
+    unitLengthX / 2,
+    unitLengthY / 2,
+    ballRadius,
     {
         label: 'ball'
     }
